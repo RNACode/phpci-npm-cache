4e5ba0a12764f4d2930a54fd772cd424bf630ec2
Nick <N.Romaniuk@software.travel>
2016-05-25 13:06:33 +0300

    added npm plugin

---
+++ src/Npm.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace RNACode\PHPCI;
+
+use PHPCI\Builder;
+use PHPCI\Model\Build;
+use PHPCI\Plugin;
+use Psr\Log\LogLevel;
+
+class Npm implements Plugin
+{
+    protected $directory;
+    protected $phpci;
+    protected $build;
+    protected $npm;
+    protected $command;
+    protected $flags;
+
+    /**
+     * Standard Constructor
+     *
+     * $options['command']
+     * $options['flags']
+     *
+     * @param Builder $phpci
+     * @param Build $build
+     * @param array $options
+     */
+    public function __construct(Builder $phpci, Build $build, array $options = [])
+    {
+        $path = $phpci->buildPath;
+        $this->build = $build;
+        $this->phpci = $phpci;
+        $this->directory = $path;
+        $this->npm = $this->phpci->findBinary('npm');
+        if (array_key_exists('directory', $options)) {
+            $this->directory = $path . '/' . $options['directory'];
+        }
+        $this->command = array_key_exists('command', $options) ? $options['command'] : null;
+        $this->flags = array_key_exists('flags', $options) ? $options['flags'] : [];
+    }
+
+    /**
+     * Executes bower and runs a specified command (e.g. install / update)
+     */
+    public function execute()
+    {
+        // check command
+        if ($this->command === null) {
+            $this->build->setStatus(Build::STATUS_FAILED);
+            $this->phpci->log('Missing require "command" parameter', LogLevel::ERROR);
+            return false;
+        }
+        $flags = $this->flags;
+        foreach ($flags as $key => $flag) {
+            $flag = trim($flag);
+            if ($flag !== '--no-color') {
+                $flags[$key] = $flag;
+            }
+        }
+        $flags[] = '--no-color';
+        $cmd = 'cd';
+        if (IS_WIN) {
+            $cmd .= ' /d';
+        }
+        $cmd .= ' %s && %s %s %s';
+        // and execute it
+        return $this->phpci->executeCommand($cmd, $this->directory, $this->npm, $this->command, implode(' ', $flags));
+    }
+
+}
